ef7317b31cbe78f9fe635e22b0dbafb36f8c7a2b
Davide Girardi <1390902+girodav@users.noreply.github.com>
2026-08-19 17:04:51 +0100

    Move sysctl hardening to gondor only

--- nixos/hosts/gondor/configuration.nix
+++ nixos/hosts/gondor/configuration.nix
@@ -16,6 +16,18 @@
   security.sudo.wheelNeedsPassword = lib.mkForce true;
   services.fail2ban.enable = true;
 
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.all.rp_filter"              = 1;
+    "net.ipv4.conf.default.rp_filter"          = 1;
+    "net.ipv4.tcp_syncookies"                  = 1;
+    "net.ipv4.conf.all.accept_redirects"       = 0;
+    "net.ipv4.conf.default.accept_redirects"   = 0;
+    "net.ipv6.conf.all.accept_redirects"       = 0;
+    "net.ipv4.conf.all.send_redirects"         = 0;
+    "net.ipv4.icmp_echo_ignore_broadcasts"     = 1;
+    "kernel.dmesg_restrict"                    = 1;
+  };
+
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
   boot.loader.grub.enable = true;
--- nixos/modules/common.nix
+++ nixos/modules/common.nix
@@ -34,19 +34,6 @@
     };
   };
 
-  # Kernel network hardening -----------------------------------------------
-  boot.kernel.sysctl = {
-    "net.ipv4.conf.all.rp_filter"              = 1;
-    "net.ipv4.conf.default.rp_filter"          = 1;
-    "net.ipv4.tcp_syncookies"                  = 1;
-    "net.ipv4.conf.all.accept_redirects"       = 0;
-    "net.ipv4.conf.default.accept_redirects"   = 0;
-    "net.ipv6.conf.all.accept_redirects"       = 0;
-    "net.ipv4.conf.all.send_redirects"         = 0;
-    "net.ipv4.icmp_echo_ignore_broadcasts"     = 1;
-    "kernel.dmesg_restrict"                    = 1;
-  };
-
   # Nix --------------------------------------------------------------------
   nix.settings = {
     experimental-features = "nix-command flakes";
